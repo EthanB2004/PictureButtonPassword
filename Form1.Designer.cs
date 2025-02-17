24b07a4cd4d48be0724111eb2ecab7d20163d8c2
SANDHILLS\lh178-10 <jad.talbert@gmail.com>
2025-02-17 12:20:32 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace PictureButtonPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.pwLabel = new System.Windows.Forms.Label();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.logInButton = new System.Windows.Forms.Button();
+            this.maleniaPictureBox = new System.Windows.Forms.PictureBox();
+            this.malekithPictureBox = new System.Windows.Forms.PictureBox();
+            this.radahnPictuerBox = new System.Windows.Forms.PictureBox();
+            this.rennalaPictureBox = new System.Windows.Forms.PictureBox();
+            this.radagonPictureBox = new System.Windows.Forms.PictureBox();
+            this.godfreyPictureBox = new System.Windows.Forms.PictureBox();
+            this.messmerPictureBox = new System.Windows.Forms.PictureBox();
+            this.rykardPictureBox = new System.Windows.Forms.PictureBox();
+            this.rellanaPictureBox = new System.Windows.Forms.PictureBox();
+            this.ranniPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.maleniaPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malekithPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radahnPictuerBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rennalaPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radagonPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.godfreyPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.messmerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rykardPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rellanaPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranniPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(458, 342);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(327, 345);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(84, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Insert Username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordLabel.Location = new System.Drawing.Point(458, 376);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(100, 23);
+            this.passwordLabel.TabIndex = 2;
+            // 
+            // pwLabel
+            // 
+            this.pwLabel.AutoSize = true;
+            this.pwLabel.Location = new System.Drawing.Point(358, 386);
+            this.pwLabel.Name = "pwLabel";
+            this.pwLabel.Size = new System.Drawing.Size(53, 13);
+            this.pwLabel.TabIndex = 3;
+            this.pwLabel.Text = "Password";
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.Location = new System.Drawing.Point(330, 437);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(75, 23);
+            this.signUpButton.TabIndex = 4;
+            this.signUpButton.Text = "Sign Up";
+            this.signUpButton.UseVisualStyleBackColor = true;
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(458, 437);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(75, 23);
+            this.logInButton.TabIndex = 5;
+            this.logInButton.Text = "Log In";
+            this.logInButton.UseVisualStyleBackColor = true;
+            // 
+            // maleniaPictureBox
+            // 
+            this.maleniaPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maleniaPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("maleniaPictureBox.Image")));
+            this.maleniaPictureBox.Location = new System.Drawing.Point(246, 23);
+            this.maleniaPictureBox.Name = "maleniaPictureBox";
+            this.maleniaPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.maleniaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.maleniaPictureBox.TabIndex = 6;
+            this.maleniaPictureBox.TabStop = false;
+            this.maleniaPictureBox.Click += new System.EventHandler(this.maleniaPictureBox_Click);
+            // 
+            // malekithPictureBox
+            // 
+            this.malekithPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.malekithPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("malekithPictureBox.Image")));
+            this.malekithPictureBox.Location = new System.Drawing.Point(103, 23);
+            this.malekithPictureBox.Name = "malekithPictureBox";
+            this.malekithPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.malekithPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.malekithPictureBox.TabIndex = 7;
+            this.malekithPictureBox.TabStop = false;
+            this.malekithPictureBox.Click += new System.EventHandler(this.malekithPictureBox_Click);
+            // 
+            // radahnPictuerBox
+            // 
+            this.radahnPictuerBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.radahnPictuerBox.Image = ((System.Drawing.Image)(resources.GetObject("radahnPictuerBox.Image")));
+            this.radahnPictuerBox.Location = new System.Drawing.Point(383, 23);
+            this.radahnPictuerBox.Name = "radahnPictuerBox";
+            this.radahnPictuerBox.Size = new System.Drawing.Size(100, 141);
+            this.radahnPictuerBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.radahnPictuerBox.TabIndex = 8;
+            this.radahnPictuerBox.TabStop = false;
+            this.radahnPictuerBox.Click += new System.EventHandler(this.radahnPictuerBox_Click);
+            // 
+            // rennalaPictureBox
+            // 
+            this.rennalaPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rennalaPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("rennalaPictureBox.Image")));
+            this.rennalaPictureBox.Location = new System.Drawing.Point(521, 23);
+            this.rennalaPictureBox.Name = "rennalaPictureBox";
+            this.rennalaPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.rennalaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rennalaPictureBox.TabIndex = 9;
+            this.rennalaPictureBox.TabStop = false;
+            this.rennalaPictureBox.Click += new System.EventHandler(this.rennalaPictureBox_Click);
+            // 
+            // radagonPictureBox
+            // 
+            this.radagonPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.radagonPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("radagonPictureBox.Image")));
+            this.radagonPictureBox.Location = new System.Drawing.Point(383, 184);
+            this.radagonPictureBox.Name = "radagonPictureBox";
+            this.radagonPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.radagonPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.radagonPictureBox.TabIndex = 10;
+            this.radagonPictureBox.TabStop = false;
+            this.radagonPictureBox.Click += new System.EventHandler(this.radagonPictureBox_Click);
+            // 
+            // godfreyPictureBox
+            // 
+            this.godfreyPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.godfreyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("godfreyPictureBox.Image")));
+            this.godfreyPictureBox.Location = new System.Drawing.Point(103, 184);
+            this.godfreyPictureBox.Name = "godfreyPictureBox";
+            this.godfreyPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.godfreyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.godfreyPictureBox.TabIndex = 11;
+            this.godfreyPictureBox.TabStop = false;
+            this.godfreyPictureBox.Click += new System.EventHandler(this.godfreyPictureBox_Click);
+            // 
+            // messmerPictureBox
+            // 
+            this.messmerPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messmerPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("messmerPictureBox.Image")));
+            this.messmerPictureBox.Location = new System.Drawing.Point(246, 184);
+            this.messmerPictureBox.Name = "messmerPictureBox";
+            this.messmerPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.messmerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.messmerPictureBox.TabIndex = 12;
+            this.messmerPictureBox.TabStop = false;
+            this.messmerPictureBox.Click += new System.EventHandler(this.messmerPictureBox_Click);
+            // 
+            // rykardPictureBox
+            // 
+            this.rykardPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rykardPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("rykardPictureBox.Image")));
+            this.rykardPictureBox.Location = new System.Drawing.Point(672, 184);
+            this.rykardPictureBox.Name = "rykardPictureBox";
+            this.rykardPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.rykardPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rykardPictureBox.TabIndex = 13;
+            this.rykardPictureBox.TabStop = false;
+            this.rykardPictureBox.Click += new System.EventHandler(this.rykardPictureBox_Click);
+            // 
+            // rellanaPictureBox
+            // 
+            this.rellanaPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rellanaPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("rellanaPictureBox.Image")));
+            this.rellanaPictureBox.Location = new System.Drawing.Point(672, 23);
+            this.rellanaPictureBox.Name = "rellanaPictureBox";
+            this.rellanaPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.rellanaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rellanaPictureBox.TabIndex = 14;
+            this.rellanaPictureBox.TabStop = false;
+            this.rellanaPictureBox.Click += new System.EventHandler(this.rellanaPictureBox_Click);
+            // 
+            // ranniPictureBox
+            // 
+            this.ranniPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ranniPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("ranniPictureBox.Image")));
+            this.ranniPictureBox.Location = new System.Drawing.Point(521, 184);
+            this.ranniPictureBox.Name = "ranniPictureBox";
+            this.ranniPictureBox.Size = new System.Drawing.Size(100, 141);
+            this.ranniPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ranniPictureBox.TabIndex = 15;
+            this.ranniPictureBox.TabStop = false;
+            this.ranniPictureBox.Click += new System.EventHandler(this.ranniPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 550);
+            this.Controls.Add(this.ranniPictureBox);
+            this.Controls.Add(this.rellanaPictureBox);
+            this.Controls.Add(this.rykardPictureBox);
+            this.Controls.Add(this.messmerPictureBox);
+            this.Controls.Add(this.godfreyPictureBox);
+            this.Controls.Add(this.radagonPictureBox);
+            this.Controls.Add(this.rennalaPictureBox);
+            this.Controls.Add(this.radahnPictuerBox);
+            this.Controls.Add(this.malekithPictureBox);
+            this.Controls.Add(this.maleniaPictureBox);
+            this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.pwLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.maleniaPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.malekithPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radahnPictuerBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rennalaPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radagonPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.godfreyPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.messmerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rykardPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rellanaPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranniPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label pwLabel;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.PictureBox maleniaPictureBox;
+        private System.Windows.Forms.PictureBox malekithPictureBox;
+        private System.Windows.Forms.PictureBox radahnPictuerBox;
+        private System.Windows.Forms.PictureBox rennalaPictureBox;
+        private System.Windows.Forms.PictureBox radagonPictureBox;
+        private System.Windows.Forms.PictureBox godfreyPictureBox;
+        private System.Windows.Forms.PictureBox messmerPictureBox;
+        private System.Windows.Forms.PictureBox rykardPictureBox;
+        private System.Windows.Forms.PictureBox rellanaPictureBox;
+        private System.Windows.Forms.PictureBox ranniPictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureButtonPassword
+{
+    
+    public partial class Form1 : Form
+    {
+        const string MALENIA = "3649";
+        const string MALIKITH = "1702";
+        const string RADAHN = "8581";
+        const string RENNALA = "5289";
+        const string RADAGON = "6430";
+        const string GODFREY = "9757";
+        const string MESSMER = "0421";
+        const string RYKARD = "2640";
+        const string RANNI = "4015";
+        const string RELLANA = "7323";
+        string passWord = "";
+        int Counter = 0;
+        string textbox_string = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+      
+        private void maleniaPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += MALENIA;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void rennalaPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += RENNALA;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void malekithPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += MALIKITH;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void radahnPictuerBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += RADAHN;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void messmerPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += MESSMER;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void rellanaPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += RELLANA;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void ranniPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += RANNI;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void godfreyPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += GODFREY;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void rykardPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            passWord += RYKARD;
+            textbox_string += "*";
+            passwordLabel.Text = textbox_string;
+        }
+
+        private void radagonPictureBox_Click(object sender, EventArgs e)
+        {
+            Counter++; 
+            passWord += RADAGON;
+          
+            passwordLabel.Text = textbox_string;
+            textbox_string += "*";
+        }
+    }
+}
